28b044149597c6349342a33c4a9a28ebf6ddf1f4
Adu <1048394082@qq.com>
2025-10-21 19:34:06 +0800

    Update gen_config.js

--- gen_config.js
+++ gen_config.js
@@ -1,6 +1,9 @@
-import fetch from "node-fetch";
+// 使用 import 会报错 SyntaxError: Cannot use import statement outside a module
+// import fetch from "node-fetch";
 
 console.log("hello gen_config.js!");
+
+const fetch = require("node-fetch")
 
 // 获取 json 文件
 fetch('./tools-config.json')
