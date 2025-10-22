d5c49203aec73d5ffedc38071a5189d1fbbdc239
Adu <1048394082@qq.com>
2025-10-22 20:06:04 +0800

    Update py_gen_config.py

--- py_gen_config.py
+++ py_gen_config.py
@@ -4,6 +4,9 @@ import json
 
 print('hello py_gen_config!')
 
+# python os函数遍历同目录下的 pages 目录下的所有文件夹和文件，按照文件夹进行分类处理，解析其中的 html 文件内容
+print(os.getcwd())
+print(os.listdir("pages"))
 
 tools = []
 configs = {"tools": tools}
@@ -22,12 +25,14 @@ def gen_config():
                   title = soup.title.string
                   print(title)
   
-                  # 获取 keywords
-                  keywords = soup.find("meta", attrs={"name": "keywords"})["content"]
+                  # 获取 keywords, 判断是否存在，不存在则返回空
+                  keywords = soup.find("meta", attrs={"name": "keywords"})
+                  keywords = keywords["content"] if keywords else ""
                   print(keywords)
   
                   # 获取 description
-                  description = soup.find("meta", attrs={"name": "description"})["content"]
+                  description = soup.find("meta", attrs={"name": "description"})
+                  description = description["content"] if description else ""
                   print(description)
   
                   # # 获取 favicon
@@ -35,11 +40,13 @@ def gen_config():
                   # print(icon)
   
                   # 获取 favicon
-                  icon = soup.find("meta", attrs={"name": "icon"})["content"]
+                  icon = soup.find("meta", attrs={"name": "icon"})
+                  icon = icon["content"] if icon else ""
                   print(icon)
   
                   # 获取功能 feture 并根据，转成字符串 list
-                  features = soup.find("meta", attrs={"name": "features"})["content"]
+                  features = soup.find("meta", attrs={"name": "features"})
+                  features = features["content"] if features else ""
                   features = features.split("，")
                   print(features)
   
